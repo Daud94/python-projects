595670a0ab5f75f91ebed66a9cd8e31d51018912
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2023-01-29 09:40:59 +0100

    psycopg2 connection

--- class-demo/demo.py
+++ class-demo/demo.py
@@ -1,1 +1,19 @@
 import psycopg2
+
+connection = psycopg2.connect('dbname=example user=postgres password=85037279af')
+
+cursor = connection.cursor()
+cursor.execute('''
+    CREATE TABLE table2 (
+        id INTEGER PRIMARY KEY,
+        completed BOOLEAN NOT NULL DEFAULT FALSE
+    );
+''')
+
+cursor.execute('''
+    INSERT INTO table2 (id, completed) VALUES (1,true);
+''')
+
+connection.commit()
+connection.close()
+cursor.close()
---
+++ practice/python_script.py
@@ -0,0 +1,38 @@
+## class
+# class Egg:
+#     def __init__(self, kind):
+#         self.kind = kind
+#
+#     def whatkind(self):
+#         return self.kind
+
+import re
+
+def main():
+    # instance of a class
+    # fried = Egg("fried")
+    # print(fried.whatkind())
+
+    ## dictionary
+    # d = {"one": 1, "two": 2, "three": 3, "four": 4}
+    # for i in d:
+    #     print(i, d[i])
+    # print("\n")
+    #
+    # # sorting by keys
+    # for i in sorted(d.keys()):
+    #     print(i, d[i])
+    #
+    # x = dict(
+    #     one=1, two=2, three=3, four=4, five=5
+    # )
+    # x["three"] = 6
+    # x.update(three=9)
+    # print(x["three"])
+
+    ## conditionals
+    print('civic'.strip('vci'))
+
+
+if __name__ == "__main__":
+    main()
